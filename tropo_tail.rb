64ffbca581fc037563132f68b708aabd10121b2c
jgisin <jgisin@gmail.com>
2016-07-17 20:34:53 -0600

    Allow full or truncated logs

--- tropo_tail.rb
+++ tropo_tail.rb
@@ -28,6 +28,8 @@ class TropoTail
       system 'stty echo'
       exit
     end
+    puts "Input 1 for full Tropo logs or 2 for truncated logs"
+    @log_display = gets.chomp
   end
 
   def get_latest
@@ -41,14 +43,22 @@ class TropoTail
 
   def tail
     maxline = 0
-    while 0 < 1
+    exit_state = 1
+    while 0 < exit_state
       linecount = 0
       @ftp.gettextfile(@filename, nil) do |line|
         info = /(?<=PRISM )(\d{7}\/\d{7}\/)(\w{32}\/|0\/)(\w{32}\/)(1\/)(\S{1,50}\/)(\[\S{1,50}\])/.match(line).to_s.yellow
         linecount += 1
         if linecount > maxline
           maxline = linecount
-          puts "#{line[0..19].blue}#{line[21..50].yellow} #{info}#{line[(info.length + 38)..-1]}"
+          if @log_display == "1"
+            puts "#{line[0..19].blue}#{line[21..50].yellow} #{info}#{line[(info.length + 38)..-1]}"
+          elsif @log_display == "2"
+            puts "#{line[0..19].blue} #{line[(info.length + 38)..-1]}"
+          else
+            puts "invalid log display option"
+            return false
+          end
         end
       end
       sleep 3
